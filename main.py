36002114edf3b2cedb48dff4f899e0a0cd9ebdf0
emi7718 <emilioair@hotmail.com>
2022-10-05 17:51:35 -0300

    dfg

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import kivy
-kivy.require('1.10.0')
-
 from kivy.app import App
-from kivy.uix.button import Label
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
-class HelloKivy(App):
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+        
+
+class MyApp(App):
 
     def build(self):
-        return Label(text ='Hello Geeks')
+        return LoginScreen()
 
-helloKivy = HelloKivy()
 
-helloKivy.run
+if __name__ == '__main__':
+    MyApp().run()
